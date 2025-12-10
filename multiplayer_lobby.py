cb5f026de2735f4ba9a505ab96c425015e6bc307
KUNOIIV <dyankey@protonmail.com>
2025-12-10 23:53:25 +0000

    multiplayer_lobby.py

--- multiplayer_lobby.py
+++ multiplayer_lobby.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template_string
+#Created my own multipleplayer local server to test and break for QA purposes
+
+from flask import Flask, render_template_string 
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+#Plain white HTML page to enter player name to enter the server
 HTML = """
 <!DOCTYPE html>
 <html>
@@ -34,10 +37,10 @@ HTML = """
 def index():
     return render_template_string(HTML)
 
-@socketio.on('message')
+@socketio.on('message') #this is to see in terminal when players has entered the server, the backend picks up the data and shows player has successfully joined
 def handle_message(data):
     print('Received:', data)
     emit('message', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True) #this is to create my own local multipleplayer server
